9d846af5b3142c40c93dc31a6487a312a8ff8f31
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-03-11 17:44:39 +0200

    done navbar and started do types,ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Navbar from '@/components/navbar'
+import { useState } from 'react';
+import { SelectedPage } from '@/shared/types';
+
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
   return (
     <div className="app bg-pink-20 bg-">
-<Navbar/>
+      <Navbar
+      selectedPage={selectedPage } setSelectedPage={setSelectedPage }/>
     </div>);
 }
 
