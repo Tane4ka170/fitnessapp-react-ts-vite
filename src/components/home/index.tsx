a2f0fa619ec7ac1cad65a92f7e3e6eb065ec6591
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-03-13 16:43:48 +0200

    Finished Home, added framer motion

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,12 +1,16 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
+
+
 import { SelectedPage } from "@/shared/types"
+import useMediaQuery from "@/hooks/useMediaQuery";
+
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
-import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
-import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
     setSelectedPage:(value: SelectedPage) => void
@@ -17,40 +21,46 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
       <section id="home" className=" gap-16 bg-pink-20 py-10 md:h-full md:pb-0">
           {/* Image and header */}
-          <div className="md:flex">
+          <motion.div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6" onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
               {/* Main header */}
-              <div>
+              <div className=" z-10 mt-32 md:basis-3/5">
                   {/* Headings */}
-                  <div>
-                      <div>
-                        <div>
+                  <motion.div className=" md:-mt-20" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{
+                      hidden: { opacity: 0, x: -50 },
+                visible:{opacity:1, x:0},  }}>
+                      <div className=" relative">
+                        <div className=" before:absolute before:-top-20 md:before:content-evolvetext before:z-[-1]">
                             <img src={HomePageText} alt="home-page-text" />
                         </div>
                       </div>
-                      <p>Ultimate Fitness Center. Exceptional Training Programs. State-of-the-art Studios for Achieving Your Dream Physique.
+                      <p className=" mt-8 text-sm md:text-start">Ultimate Fitness Center. Exceptional Training Programs. State-of-the-art Studios for Achieving Your Dream Physique.
                           Transform Your Body Today.</p>
-                  </div>
+                  </motion.div>
                   {/* Actions */}
-                  <div>
+                  <motion.div className="mt-8 flex items-center gap-8" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ delay:0.2, duration: 0.5 }} variants={{
+                      hidden: { opacity: 0, x: -50 },
+                visible:{opacity:1, x:0},  }}>
                       <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                       <AnchorLink className=" text-sm font-bold text-primary-500 underline hover:text-secondary-500" onClick={() => setSelectedPage(SelectedPage.ContactUs)} href={`#${SelectedPage.ContactUs}`}>
                           <p>Learn more</p>
                       </AnchorLink>
-                  </div>
+                  </motion.div>
               </div>
               {/* image */}
-              <div>
+              <div className=" flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                  <img src={HomePageGraphic} alt="home-pageGraphic" />
               </div>
 
-          </div>
+          </motion.div>
           {/* Sponsors */}
           {isAboveMediumScreens && (
-              <div>
-                  <div>
-                      <img src={SponsorRedBull} alt="redbull" />
-                      <img src={SponsorForbes} alt="forbes" />
-                      <img src={SponsorFortune} alt="fortune" />
+              <div className="h-[150px] w-full bg-primary-100 py-10">
+                  <div className="mx-auto w-5/6">
+                      <div className="flex w-3/5 items-center justify-between gap-8">
+                          <img src={SponsorRedBull} alt="redbull" />
+                          <img src={SponsorForbes} alt="forbes" />
+                          <img src={SponsorFortune} alt="fortune" />
+                      </div>
                   </div>
               </div>
           )}
